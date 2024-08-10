57e0d6ddbf2fdae2143bea15d2186ec9d261a00f
Joe Warren <hungryjoe.warren@gmail.com>
2024-08-10 11:25:53 +0100

    tweak queen top

--- src/Queen.hs
+++ src/Queen.hs
@@ -8,14 +8,14 @@ import Linear.V2 (V2(..))
 
 xSection :: Double -> Waterfall.Path2D
 xSection radius = 
-    let bobbleR = radius * 0.1
+    let bobbleR = radius * 0.25
         h1 = radius * 1.5 - bobbleR
         hMid = h1 * 0.75
       in Waterfall.pathFrom zero
             [ Waterfall.lineTo (V2 (radius * 0.8) 0)
             , Waterfall.bezierTo (V2 (radius * 0.9) (hMid * 0.25)) (V2 radius (hMid * 0.75)) (V2 radius hMid )
             , Waterfall.bezierTo (V2 radius ((h1 + hMid) / 2 )) (V2 (radius * 0.5) h1) (V2 (bobbleR * 0.6) h1) 
-            , Waterfall.arcViaRelative (V2 bobbleR (bobbleR * 0.8)) (V2 (-bobbleR * 0.6) (bobbleR * 1.8))
+            , Waterfall.arcViaRelative (V2 (bobbleR * 0.4) (bobbleR * 0.8)) (V2 (-bobbleR * 0.6) (bobbleR * 1.8))
             ]
 
 topper :: Double -> Waterfall.Solid
